77d12c7b77e6b7fbb9ad09c0cb1c004ea64e5dc9
Johny Noise <79932410+johnynoise@users.noreply.github.com>
2025-05-24 21:20:25 -0400

    Atualizando arquivo de rotas de autenticação

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 auth_router = APIRouter(prefix="/auth",tags=["auth"])
 
-auth_router.get("/")
+@auth_router.get("/")
 async def autenticar():
     return {"message": "Voce está autenticado", "autenticado": False}
