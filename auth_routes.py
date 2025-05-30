6d0f7491e74436a7bb9f513d46f182cb72fb4b2b
Johny Noise <79932410+johnynoise@users.noreply.github.com>
2025-05-29 22:15:00 -0400

    ✨ feat: Configuração de Depends
    criação de um arquivo para iniciar a sessão no  banco de dados e finalizar a sessão.

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from dependecies import pegar_sessao
+from models import db, Usuario
+
 
 auth_router = APIRouter(prefix="/auth",tags=["auth"])
 
 @auth_router.get("/")
 async def autenticar():
     return {"message": "Voce está autenticado", "autenticado": False}
+
+@auth_router.post("/criar_conta") 
+async def registro(nome: str, email: str, senha: str, session=Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if usuario:
+        return {"message": "Email já cadastrado"}
+    # Verifica se o email já está cadastrado
+    else:
+        novo_usuario = Usuario(nome=nome, email=email, senha=senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {"message": "Usuário criado com sucesso", "email": email}
